349136f27a86174fe3361fb219f6bec7852cf789
David Estephanian <desteph@protonmail.com>
2022-04-18 23:48:28 -0700

    started creating schema

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+// require('dotenv').config();
 
 const poolConfig = process.env.DATABASE_URL ? {
   connectionString: process.env.DATABASE_URL,
--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,18 @@
--- TBC --
+DROP DATABASE IF EXISTS qa;
+CREATE DATABASE qa;
+
+CREATE TABLE question_overview (
+  id INTEGER PRIMARY KEY NOT NULL,
+);
+
+-- SERIAL data type allows you to automatically generate unique integer numbers (IDs, identity, auto-increment, sequence) for a column.
+CREATE TABLE question_info (
+  question_id SERIAL PRIMARY KEY NOT NULL,
+  product_id INTEGER REFERENCES question_overview(id),
+  question_body VARCHAR(100),
+  -- question_date VARCHAR(24),
+  asker_name: VARCHAR(50),
+                            -- DEAFAULT NULL?
+  question_helpfulness INTEGER NOT NULL,
+  reported BOOLEAN
+)

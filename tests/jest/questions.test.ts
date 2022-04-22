3923b56ba94e8c8fa381dec97d8e4f0c9497d508
David Estephanian <desteph@protonmail.com>
2022-04-22 11:33:07 -0700

    recent push

--- tests/jest/questions.test.ts
+++ tests/jest/questions.test.ts
@@ -5,10 +5,24 @@ const app = createServer();
 
 describe('questions', () => {
   describe('get questions route', () => {
-    describe('given query with no params', () => {
+    describe('given questions with no params', () => {
       it('should return a 404 response for /questions', async () => {
-        const { statusCode } = await supertest(app).get('/questions');
-        expect(statusCode).toBe(404);
+        const response = await supertest(app).get('/questions');
+        expect(response.status).toBe(404);
+      });
+      it('should return \'Error: invalid product id provided\'', async () => {
+        const response = await supertest(app).get('/questions');
+        expect(response.text).toEqual('Error: invalid product id provided');
+      });
+    })
+    describe('given questions with a product id param', () => {
+      it('should return a 200 response for a product id in bounds /questions?product_id=[numInBounds]', async () => {
+        const response = await supertest(app).get('/questions?product_id=963063');
+        expect(response.status).toBe(200);
+      });
+      it('should return a 404 response for product id greater than database', async () => {
+        const response = await supertest(app).get('/questions?product_id=9999999999');
+        expect(response.status).toEqual(404);
       });
     })
   })
